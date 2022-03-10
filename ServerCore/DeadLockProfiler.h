44690f91735e11bb0005178e7e804d0bc5d154ed
jidungg <51319585+jidungg@users.noreply.github.com>
2022-03-11 03:30:32 +0900

    feat : DeadLockProfiler 기능 추가, 데드락 현상 수정
    데드락이 존재할 수 있는지 검사하는 DeadLockProfiler 추가
    GameServer 프로젝트 실행 시락 오류 수정(RefCounting.h)

--- ServerCore/DeadLockProfiler.h
+++ ServerCore/DeadLockProfiler.h
@@ -8,9 +8,10 @@ class DeadLockProfiler
 public :
 	void PushLock(const char* name);
 	void PopLock(const char* name);
+	void CheckCycle();
 
 private:
-	void CheckCycle();
+
 	void DFS(int32 index);
 
 	unordered_map<const char*, int32> _nameToId; //lock의 ID를 보관
@@ -22,7 +23,7 @@ private:
 
 	//DFS 관련
 	vector<int32> _discoveredOrder; //노드가 발견된 순서를 기록
-	int32 _discoverdCount = 0; //노드가 발견된 순서 추적
+	int32 _discoveredCount = 0; //노드가 발견된 순서 추적
 	vector<bool> _finished; //DFS가 종료됐는지 확인
 	vector<int32> _parent; //부모님이 누구인지
 
