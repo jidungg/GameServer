e5aeafd6ae71ed706f67ee542665998f697e3445
unknown <akrhwkwk@gmail.com>
2022-04-25 05:15:08 +0900

    Feat: Server와 Client TCP소켓 연결
    기본적인 TCP 소켓 연결

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -1,9 +1,45 @@
 ﻿#include "pch.h"
-
 #include <iostream>
+
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-    //HelloWorld();
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		return 0;
+	}
+
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, NULL);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket errCode : " << errCode << endl;
+		return 0;
+	}
+
+	SOCKADDR_IN serverAddr; 
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1"); //deprecated, same as next line code
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);
+
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Connect errCode : " << errCode << endl;
+		return 0;
+	}
+
+	//Do Something
+
+	::closesocket(clientSocket);
+
+	::WSACleanup();
 }
 
