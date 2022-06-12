6d5d76c74b6c08c5abb88c145d0bc87caaf91d49
unknown <akrhwkwk@gmail.com>
2022-06-13 06:43:34 +0900

    feat: IOCP 기본 구조 추가

--- ServerCore/NetAddress.h
+++ ServerCore/NetAddress.h
@@ -11,7 +11,7 @@ public :
 	uint16 GetPort() { return ::ntohs(_sockAddr.sin_port); }
 
 public :
-	static IN_ADDR Ip2Addrss(const WCHAR* ip);
+	static IN_ADDR Ip2Address(const WCHAR* ip);
 private:
 	SOCKADDR_IN _sockAddr = {};
 };
